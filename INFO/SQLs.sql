1c75c781174082ed076f3b23ff8ff2ea6843b821
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-06-11 20:05:58 +0300

    init app

---
+++ INFO/SQLs.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE db;
+
+CREATE TABLE IF NOT EXISTS places
+( id INTEGER NOT NULL AUTO_INCREMENT,
+  image VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO
+	places (image, name, description)
+VALUES
+	('azure-beach.jpg', 'Azure beach', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+	('sunny-club.jpg', 'Sunny Club', 'Donec tempor mi nulla, eu aliquam massa rutrum eu.'),
+	('fish-farm.jpg', 'Fish farm', 'Nam tempus ultrices mauris.');
+
+
